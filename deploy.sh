4071a371c4a4338705f38bf633a74ab7d12dc561
Rafael Fernandez <rafael.fernandez.ve@gmail.com>
2019-02-21 11:38:33 -0500

    modificacion en shell script para despliegue local

---
+++ deploy.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+############################################################################
+#title           :deploy.sh
+#description     :Este script es para hacer build y desplegar en docker (develop) .
+#author		 : Rafael Fernandez
+#date            :20190220
+#version         :0.1
+#usage		 :sh deploy.sh
+#notes           :Install docker y java, java y maven para usar este script.
+#                :las variables en el file setvar son de desarrollo
+#==============================================================================
+
+#READ PROFILE TO DEPLOY
+echo "PROFILE:"
+read PROFILE
+
+
+#SET ENV VARS
+export $(grep -v '^#' setvar-$PROFILE | xargs)
+
+# BUILD CODE 
+./mvnw clean
+./mvnw install -DskipTests
+
+#BUILD IMAGE
+docker build -t $DOCKER_IMAGE .
+
+# RUN CONTAINER IN DOCKER
+CID=$(docker run -d --network=$NETWORK --env-file setvar-dev $DOCKER_IMAGE)
+IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CID)
+GATEWAY=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.Gateway}}{{end}}' $CID)
+
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] DEPLOYED (INFO CONTAINER)"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] Docker Image: $DOCKER_IMAGE"
+echo "[INFO] Id Container: $CID"
+echo "[INFO] Network: $NETWORK "
+echo "[INFO] IP Container: $IP"
+echo "[INFO] Gateway: $GATEWAY"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO]"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] SPRING CLOUD CONFIGURATION SERVER (INFO)"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] Git Repository: $GIT_REPOSITORY_URI"
+echo "[INFO] User: $GIT_USERNAME"
+echo "[INFO] Password: ********"
+echo "[INFO] Security: true"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] CONSUL CLINT DISCOVERY (INFO)"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO]	CONSUL HOST: $CONSUL_SERVER"
+echo "[INFO]	CONSUL PORT: $CONSUL_PORT"
